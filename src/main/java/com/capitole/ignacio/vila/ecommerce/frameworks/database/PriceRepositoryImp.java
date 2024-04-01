6e2a4a44e8dda28a0ebdb54ceb0c0a3dd136a76d
Ignacio Vila <ivila@dc.uba.ar>
2024-04-01 14:53:15 -0300

    simplify

--- src/main/java/com/capitole/ignacio/vila/ecommerce/frameworks/database/PriceRepositoryImp.java
+++ src/main/java/com/capitole/ignacio/vila/ecommerce/frameworks/database/PriceRepositoryImp.java
@@ -5,7 +5,6 @@ import com.capitole.ignacio.vila.ecommerce.domain.Price;
 import com.capitole.ignacio.vila.ecommerce.usecases.PriceRepository;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +16,6 @@ public class PriceRepositoryImp implements PriceRepository {
     @Override
     public List<Price> findPrices(Long brandId, Long productId, LocalDateTime date) {
         List<PriceEntity> prices = priceRepositoryJPA.findByBrandEntity_IdAndProductIdAndStartDateLessThanAndEndDateGreaterThan(brandId, productId, date, date);
-        return prices.stream().map(priceMapper::mapToDomain).collect(Collectors.toList());
+        return prices.stream().map(priceMapper::mapToDomain).toList();
     }
 }
